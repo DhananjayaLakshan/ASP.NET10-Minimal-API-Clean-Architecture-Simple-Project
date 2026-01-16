d0957203721c364de4b4604d4d7ea3f868c45afe
DhananjayaLakshan <abey.dhananjayalakshan@gmail.com>
2026-01-16 14:12:38 +0530

    Created a simple entity

---
+++ CleanMinimalApi/CleanMinimalApi.Domain/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace CleanMinimalApi.Domain.Entities;
+
+public sealed class Product
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+}
